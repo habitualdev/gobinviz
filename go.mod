e7a39424a740721f870f95699cff18788457ad4f
habitual <habitual@habitual-media.com>
2022-05-31 08:05:55 -0400

    Big rework, hilbert curves for image processing

--- go.mod
+++ go.mod
@@ -3,18 +3,27 @@ module github.com/roaldi/gobinviz
 go 1.18
 
 require (
+	github.com/ajdnik/imghash v1.0.0
 	github.com/corona10/goimagehash v1.1.0
+	github.com/dsoprea/hilbert v0.0.0-20170129033827-2ad04e2ae9f8
 	github.com/roaldi/richdiff v0.0.0-20220529155939-aad7c696e4a1
+	github.com/saferwall/pe v1.1.9
 )
 
 require (
+	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/google/hilbert v0.0.0-20181122061418-320f2e35a565 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.3.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/r3labs/diff/v3 v3.0.0 // indirect
+	github.com/r9y9/gossp v0.0.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/steakknife/hamming v0.0.0-20180906055917-c99c65617cd3 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	go.mozilla.org/pkcs7 v0.0.0-20210826202110-33d05740a352 // indirect
 	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	google.golang.org/appengine v1.6.6 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package binviz
 
 import (
+	"github.com/ajdnik/imghash"
+	"github.com/ajdnik/imghash/hashtype"
+	"github.com/ajdnik/imghash/similarity"
 	"github.com/corona10/goimagehash"
 	"github.com/roaldi/richdiff"
+	"github.com/dsoprea/hilbert"
 	"image"
 	"image/color"
 	"image/png"
 	"log"
 	"math"
 	"os"
+	"github.com/saferwall/pe"
 )
 
-const blockSize = 150
-
 type BinViz struct {
+	BinaryBytes []byte
 	RichDiffResults richdiff.Results
 	Image image.Image
 	BinAverageHash *goimagehash.ExtImageHash
 	BinDifferenceHash *goimagehash.ExtImageHash
 	RichAverageHash *goimagehash.ImageHash
 	RichDifferenceHash *goimagehash.ImageHash
+	BlockHash hashtype.Binary
+	MedianHash hashtype.Binary
+	MarrHildeHash hashtype.Binary
+	PEInfo *pe.File
 }
 
 func (b BinViz) AverageDistance(altBinViz BinViz) int {
@@ -54,6 +62,20 @@ func (b BinViz) RichDifferenceDistance(altBinViz BinViz) int {
 	return distance
 }
 
+func (b BinViz) BlockHashDistance(altBinViz BinViz) int {
+	distance := similarity.Hamming(b.BlockHash, altBinViz.BlockHash)
+	return int(distance)
+}
+
+func (b BinViz) MedianDistance(altBinViz BinViz) int {
+	distance := similarity.Hamming(b.MedianHash, altBinViz.MedianHash)
+	return int(distance)
+}
+
+func (b BinViz) MarrHildeDistance(altBinViz BinViz) int {
+	distance := similarity.Hamming(b.MarrHildeHash, altBinViz.MarrHildeHash)
+	return int(distance)
+}
 
 func (b BinViz) SaveImage(filename string) error {
 	file, err := os.Create(filename)
@@ -70,16 +92,19 @@ func (b BinViz) SaveImage(filename string) error {
 func ProcessBinary(data []byte) (BinViz, error) {
 	var binViz BinViz
 	var err error
+	binViz.PEInfo, _ = pe.NewBytes(data, &pe.Options{})
+	binViz.BinaryBytes = data
+	binViz.PEInfo.Parse()
 	binViz.RichDiffResults, err = richdiff.RichExtraction(data)
 	if err != nil {
 		log.Println(err.Error())
 	}
-	binViz.Image, err = byteToPng(data)
+	binViz.Image, err = byteToPng(binViz)
 	if err != nil {
 		log.Println(err.Error())
 		return BinViz{}, err
 	}
-	binViz.BinAverageHash, err = goimagehash.ExtAverageHash(binViz.Image, 32, 32)
+	binViz.BinAverageHash, err = goimagehash.ExtAverageHash(binViz.Image, 16, 16)
 	if err != nil {
 		log.Println(err.Error())
 	}
@@ -87,46 +112,53 @@ func ProcessBinary(data []byte) (BinViz, error) {
 	if err != nil {
 		log.Println(err.Error())
 	}
-	binViz.RichAverageHash, err = goimagehash.AverageHash(binViz.RichDiffResults.ByteImage)
-	if err != nil {
-		log.Println(err.Error())
+	if binViz.RichDiffResults.ByteImage != nil {
+		binViz.RichAverageHash, err = goimagehash.AverageHash(binViz.RichDiffResults.ByteImage)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		binViz.RichDifferenceHash, err = goimagehash.DifferenceHash(binViz.RichDiffResults.ByteImage)
+		if err != nil {
+			log.Println(err.Error())
+		}
 	}
-	binViz.RichDifferenceHash, err = goimagehash.DifferenceHash(binViz.RichDiffResults.ByteImage)
-	if err != nil {
-		log.Println(err.Error())
-	}
+	bmhash := imghash.NewBlockMean()
+	binViz.BlockHash = bmhash.Calculate(binViz.Image)
+	medianHash := imghash.NewMedian()
+	binViz.MedianHash = medianHash.Calculate(binViz.Image)
+	marrHash := imghash.NewMarrHildreth()
+	binViz.MarrHildeHash = marrHash.Calculate(binViz.Image)
 	return binViz, nil
 }
 
-func byteToPng(data []byte) (image.Image, error) {
-	byteSize := len(data)
+func byteToPng(b BinViz) (image.Image, error) {
+	var sectionAddresses []uint32
+	byteSize := len(b.BinaryBytes)
 	sideLength := math.Sqrt(float64(byteSize))
 	imageSideLength := int(math.Ceil(sideLength))
 	sizeDiff := (imageSideLength * imageSideLength) - byteSize
 	if sizeDiff > 0 {
-		data = append(data, make([]byte, sizeDiff)...)
+		b.BinaryBytes = append(b.BinaryBytes, make([]byte, sizeDiff)...)
 	}
 	img := image.NewRGBA(image.Rect(0, 0, imageSideLength, imageSideLength))
-
-	numSideBlocks := (imageSideLength / blockSize) + 1
-	if numSideBlocks == 0 {
-		numSideBlocks = 1
+	hilbertSize := 2
+	for hilbertSize < byteSize {
+		hilbertSize *= 2
 	}
-	pix := 0
-	for i := 0; i < imageSideLength; i++ {
-		for j := 0; j < imageSideLength; j++ {
-			pix++
-			if pix > len(data) - 1 {
-				break
+	for _, section := range b.PEInfo.Sections{
+		sectionAddresses =  append(sectionAddresses, section.Header.VirtualAddress)
+	}
+	sectionStep := 255/ len(sectionAddresses)
+	sectionColor := 0
+	for i := 0; i <byteSize; i++{
+		for _, address := range sectionAddresses{
+			if i == int(address/4){
+				sectionColor = sectionColor + sectionStep
 			}
-			img.Set(i % blockSize + ((i/blockSize)%numSideBlocks)*blockSize , j%blockSize + ((j/blockSize)%numSideBlocks)*blockSize, color.RGBA{
-				uint8(data[pix]),
-				uint8((data[pix])),
-				uint8((data[pix])),
-				uint8(255),
-			})
 		}
+		h, _ := hilbert.NewHilbert64(uint64(hilbertSize))
+		x, y, _ := h.Map(uint64(i+1))
+		img.SetRGBA(int(x), int(y), color.RGBA{uint8(128 - sectionColor/2), b.BinaryBytes[i], 32, 255})
 	}
-
 	return img, nil
 }
